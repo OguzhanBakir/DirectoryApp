7df13e44e4ba02eda87ecc277ee7f06343045ae2
OguzhanBakir <oguzhanbak7@gmail.com>
2022-10-23 21:54:30 +0300

    Excel id template for generate report

---
+++ Services/DirectoryApp.Services.Report/Shared/CreateExcelMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DirectoryApp.Services.Report.Shared
+{
+    public class CreateExcelMessage
+    {
+        public Guid ReportResultId { get; set; }
+    }
+}
