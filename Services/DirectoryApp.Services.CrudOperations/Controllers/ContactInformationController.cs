4b5ab2bf18244e8ecc6ba2493a80d56aa75c4c1c
OguzhanBakir <oguzhanbak7@gmail.com>
2022-10-23 20:52:07 +0300

    Person and contact microservice controllers added

---
+++ Services/DirectoryApp.Services.CrudOperations/Controllers/ContactInformationController.cs
@@ -0,0 +1,69 @@
+﻿using DirectoryApp.Services.CrudOperations.Models;
+using DirectoryApp.Services.CrudOperations.Services;
+using DirectoryApp.Shared.ControllerBases;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DirectoryApp.Services.CrudOperations.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactInformationController : CustomBaseController
+    {
+        private readonly IContactInformationService _contactInformationService;
+
+
+        public ContactInformationController(IContactInformationService contactInformationService)
+        {
+            _contactInformationService = contactInformationService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var contactInformations = await _contactInformationService.GetAllAsync();
+
+            return CreateActionResultInstance(contactInformations);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetContactInformationByPersonId(Guid id)
+        {
+            var response = await _contactInformationService.GetContactInformationByPersonId(id);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [Route("Create")]
+        [HttpPost]
+        public async Task<IActionResult> Create(ContactInformation informationDto)
+        {
+            var response = await _contactInformationService.Save(informationDto);
+
+            return CreateActionResultInstance(response);
+        }
+
+
+        [HttpPut]
+        public async Task<IActionResult> Update(ContactInformation informationDto)
+        {
+            var response = await _contactInformationService.Update(informationDto);
+
+            return CreateActionResultInstance(response);
+        }
+
+
+        [Route("Delete/{id}")]
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var response = await _contactInformationService.Delete(id);
+
+            return CreateActionResultInstance(response);
+        }
+    }
+}
---
+++ Services/DirectoryApp.Services.CrudOperations/Controllers/PersonController.cs
@@ -0,0 +1,74 @@
+﻿using DirectoryApp.Services.CrudOperations.Data;
+using DirectoryApp.Services.CrudOperations.Models;
+using DirectoryApp.Services.CrudOperations.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DirectoryApp.Shared.ControllerBases;
+
+namespace DirectoryApp.Services.CrudOperations.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : CustomBaseController
+    {
+        private readonly IPersonService _personService;
+
+
+        public PersonController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var persons = await _personService.GetAllAsync();
+
+            return CreateActionResultInstance(persons);
+        }
+
+        
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPersonById(string id)
+        {
+            var response = await _personService.GetPersonById(id);
+
+            return CreateActionResultInstance(response);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Person personDto)
+        {
+            var response = await _personService.Save(personDto);
+
+            return CreateActionResultInstance(response);
+        }
+
+
+        [HttpPut]
+        public async Task<IActionResult> Update(Person personDto)
+        {
+            var response = await _personService.Update(personDto);
+
+            return CreateActionResultInstance(response);
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var response = await _personService.Delete(id);
+
+            return CreateActionResultInstance(response);
+        }
+
+
+    }
+}
