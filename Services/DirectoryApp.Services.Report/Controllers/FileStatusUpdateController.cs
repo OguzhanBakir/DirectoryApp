07382cc5904030c33494fd5ad8b1bd3bf8e6dcca
OguzhanBakir <oguzhanbak7@gmail.com>
2022-10-23 21:55:08 +0300

    Implemented report api controllers

---
+++ Services/DirectoryApp.Services.Report/Controllers/FileStatusUpdateController.cs
@@ -0,0 +1,61 @@
+﻿using DirectoryApp.Services.Report.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DirectoryApp.Services.Report.Controllers
+{
+    
+    [ApiController]
+    public class FileStatusUpdateController : ControllerBase
+    {
+        private readonly ReportContext _dbContext;
+
+        public FileStatusUpdateController(ReportContext dbContext)
+        {
+            _dbContext = dbContext;
+            
+        }
+
+
+        [Route("FileStatusUpdate")]
+        [HttpPost]
+        public async Task<IActionResult> StatusUpdate(Guid reportResultId,string fileName)
+        {
+           
+
+
+            var reportFile = await _dbContext.Report.SingleOrDefaultAsync(x => x.ReportResultId == reportResultId);
+
+
+
+
+            if (reportFile != null)
+            {
+
+                reportFile.CreationDateTime = DateTime.Now;
+                reportFile.FileLocation = fileName;
+                reportFile.ReportStatus = "Tamamlandı";
+
+                await _dbContext.SaveChangesAsync();
+
+                //Zaman kalırsa SignalR
+
+             
+
+
+
+
+            }
+
+
+            return Ok();
+
+        }
+    }
+}
---
+++ Services/DirectoryApp.Services.Report/Controllers/ReportResultController.cs
@@ -0,0 +1,53 @@
+﻿using DirectoryApp.Services.Report.Data;
+using DirectoryApp.Services.Report.Models;
+using DirectoryApp.Services.Report.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DirectoryApp.Shared.ControllerBases;
+
+
+namespace DirectoryApp.Services.Report.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ReportResultController : CustomBaseController
+    {
+
+        private readonly IReportResultService _reportResultService;
+
+        public ReportResultController(IReportResultService reportResultService)
+        {
+            _reportResultService = reportResultService;
+        }
+
+
+        [HttpGet]
+        [Route("GetAllReport")]
+        public async Task<IActionResult> GetAll()
+        {
+            var reportResult = await _reportResultService.GetAllAsync();
+
+            return CreateActionResultInstance(reportResult);
+        }
+
+
+
+        [HttpPost]
+        [Route("GenerateReport")]
+        public async Task<IActionResult> GenerateReport()
+        {
+            var response = await _reportResultService.GenerateReportRequest();
+
+            return CreateActionResultInstance(response);
+        }
+
+
+
+
+    }
+}
