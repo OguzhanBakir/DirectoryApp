2a09f4625e033591f29d75f42aafc0e32dd50b65
OguzhanBakir <oguzhanbak7@gmail.com>
2022-10-23 20:39:24 +0300

    Shared services added; Response types and initial configuration were added

---
+++ Shared/DirectoryApp.Shared/ControllerBases/CustomControllerBase.cs
@@ -0,0 +1,20 @@
+﻿using DirectoryApp.Shared.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DirectoryApp.Shared.ControllerBases
+{
+    public class CustomBaseController : ControllerBase
+    {
+        public IActionResult CreateActionResultInstance<T>(Response<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
---
+++ Shared/DirectoryApp.Shared/Dtos/NoContent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DirectoryApp.Shared.Dtos
+{
+    public class NoContent
+    {
+    }
+}
