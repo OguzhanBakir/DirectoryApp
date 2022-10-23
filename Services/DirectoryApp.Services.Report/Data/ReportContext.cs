aac236d35524a84b2146b92579b70c1f95cc5e27
OguzhanBakir <oguzhanbak7@gmail.com>
2022-10-23 21:51:03 +0300

    Report database context and report operations micro services interfaces

---
+++ Services/DirectoryApp.Services.Report/Data/ReportContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DirectoryApp.Services.Report.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DirectoryApp.Services.Report.Data
+{
+    public class ReportContext : DbContext
+    {
+        public ReportContext(DbContextOptions<ReportContext> options)
+        : base(options)
+        {
+
+            Database.EnsureCreated();
+        }
+
+        public DbSet<ReportResult> Report { get; set; }
+    }
+}
---
+++ Services/DirectoryApp.Services.Report/Models/ReportResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DirectoryApp.Services.Report.Models
+{
+    public class ReportResult
+    {
+        public Guid ReportResultId { get; set; }
+        public DateTime RequestDateTime { get; set; }
+        public DateTime CreationDateTime { get; set; }
+        public string ReportStatus { get; set; }
+        public string FileLocation { get; set; }
+
+    }
+}
