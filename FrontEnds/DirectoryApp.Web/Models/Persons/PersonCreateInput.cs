77ecfe04e1670c4ab0e2a84873fff66732548aa5
Oğuzhan BAKIR <oguzhanbak7@gmail.com>
2022-10-24 05:09:10 +0300

    Update PersonCreateInput.cs
    Added validations for person create model

--- FrontEnds/DirectoryApp.Web/Models/Persons/PersonCreateInput.cs
+++ FrontEnds/DirectoryApp.Web/Models/Persons/PersonCreateInput.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -8,13 +8,21 @@ namespace DirectoryApp.Web.Models.Persons
 {
     public class PersonCreateInput
     {
+        [Required(ErrorMessage = "The first name cannot be empty")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "The first name must be at least 2 characters")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "The first name only accept letters")]
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "The last name cannot be empty")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "The last name be at least 2 characters")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "The last name only accept letters")]
 
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
-
+        [Required(ErrorMessage = "The company name cannot be empty")]
+        [StringLength(int.MaxValue, MinimumLength = 5, ErrorMessage = "The company name be at least 5 characters")]
         [Display(Name = "Company Name")]
         public string Company { get; set; }
 
